f4a77c43f58b3d4af5718cf94ef01285992892b6
Florent Dubois <florent.dubois@devaddict.io>
2016-10-04 22:02:09 +0200

    Parse options in lib/config

--- index.js
+++ index.js
@@ -9,23 +9,9 @@ var spawn  = require('child_process').spawn;
 
 var express = require('express');
 var mkdirp  = require('mkdirp');
-var options = require('commander');
 var winston = require('winston');
 
 var config = require('./lib/config.js');
-
-options.version(require('./package.json').version)
-  .option('-p, --port [port]', 'Listening port for client connection [8080]', 8080)
-  .option('-x, --bin [path]', 'Path to native shell binaries [/bin/bash]', '/bin/bash')
-  .option('-H, --history [path]', 'Path to history save file [~/.broshell_history]', process.env.HOME + '/.broshell_history')
-  .option('-u, --uid [uid]', 'User identity of the bash process [node process uid]', process.getuid())
-  .option('-g, --gid [gid]', 'Group identity of the bash process [node process gid]', process.getgid())
-  .option('-P, --path [path]', 'Startup working directory [current directory]', process.cwd())
-  .option('-l, --logs [path]', 'Logs directory [/var/log/broshell]', '/var/log/broshell')
-  .option('-v, --verbose', 'Enable verbose logging')
-  .parse(process.argv);
-
-config.set(options);
 
 try {
   mkdirp.sync(config.get('logs'));
