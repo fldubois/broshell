6ebfa50aae0070ebde05aefe77c4d6759911446a
Florent Dubois <florent.dubois@devaddict.io>
2016-10-05 23:11:05 +0200

    Add lib/bash module

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 
 process.name = 'broshell';
 
-var crypto = require('crypto');
-var http   = require('http');
-var spawn  = require('child_process').spawn;
-
 var express = require('express');
 var mkdirp  = require('mkdirp');
 
 var config = require('./lib/config.js');
 var logger = require('./lib/logger.js');
+
+var Bash = require('./lib/bash.js');
 
 try {
   mkdirp.sync(config.get('logs'));
@@ -27,18 +25,6 @@ app.use(express.static('public'));
 var server = app.listen(config.get('port'), function () {
   logger.global().info('Broshell listening on port ' + config.get('port'));
 });
-
-var shasum = crypto.createHash('sha1');
-shasum.update('boundary-' + Math.random());
-var token = shasum.digest('hex');
-
-var boundary = ' echo "' + token + JSON.stringify({
-  username: "`whoami`",
-  hostname: "`hostname`",
-  cwd: "`pwd`"
-}).replace(/"/g, '\\"') + '"\n';
-
-var resetHistoryIndex = ' HISTINDEX=0;';
 
 /**
  * Generate a 4 characters short ID
@@ -59,54 +45,21 @@ io.on('connection', function (socket) {
 
   logger.session(session).info('Connection from ' + socket.request.connection.remoteAddress, {session: session});
 
-  var bash = spawn(config.get('bin'), [], {
-    uid: +config.get('uid'),
-    gid: +config.get('gid'),
-    cwd: config.get('path'),
-    env: {
-      HISTFILE: config.get('history'),
-      HISTFILESIZE: 500,
-      HISTSIZE: 500,
-      HISTCONTROL: 'ignorespace',
-      HISTINDEX: 0,
-      SHELLOPTS: 'history:histexpand'
-    }
-  });
-  var cb;
+  var bash = new Bash();
 
-  bash.stdout.on('data', function (data) {
-    data.toString().replace(/(\n)*$/, '').split('\n').forEach(function (line) {
-      if (line.indexOf(token) === 0) {
-        if (typeof cb === 'function') {
-          cb = null;
-        } else {
-          var context = JSON.parse(line.replace(token, ''));
-          logger.session(session).verbose('[context]', JSON.stringify(context), {session: session});
-          socket.emit('context', context);
-        }
-      } else {
-        logger.session(session).info('[stdout]', line, {session: session});
-
-        if (typeof cb === 'function') {
-          cb(line);
-        } else {
-          socket.emit('stdout', line);
-        }
-      }
-    });
+  bash.on('context', function (context) {
+    logger.session(session).verbose('[context]', JSON.stringify(context), {session: session});
+    socket.emit('context', context);
   });
 
-  bash.stderr.on('data', function (data) {
-    data.toString().replace(/(\n)*$/, '').split('\n').forEach(function (line) {
-      logger.session(session).info('[stderr]', line, {session: session});
-      socket.emit('stderr', line);
-    });
+  bash.on('stdout', function (line) {
+    logger.session(session).info('[stdout]', line, {session: session});
+    socket.emit('stdout', line);
   });
 
-  bash.stdin.on('error', function (err) {
-    if (err.code !== 'ECONNRESET') {
-      throw err;
-    }
+  bash.on('stderr', function (line) {
+    logger.session(session).info('[stderr]', line, {session: session});
+    socket.emit('stderr', line);
   });
 
   bash.on('close', function (code) {
@@ -116,37 +69,23 @@ io.on('connection', function (socket) {
 
   socket.on('cmd', function (cmd) {
     logger.session(session).info('[cmd]', cmd.replace(/(\n)*$/, ''), {session: session});
-    bash.stdin.write(cmd.replace(/(\n)*$/, '\n') + resetHistoryIndex + boundary);
+    bash.execute(cmd);
   });
 
   socket.on('interrupt', function () {
     logger.session(session).info('[interrupt]', {session: session});
-    spawn('pkill', ['-P', bash.pid]);
+    bash.interrupt();
   });
 
   socket.on('history', function (direction, callback) {
     logger.session(session).verbose('[history]', direction, {session: session});
-    cb = callback;
-
-    if (direction < 0) {
-      var histsize = '$(history | tail -n 1 | awk -F "[[:space:]]+" \'{print $2}\')';
-      var cmd = 'HISTINDEX=$((HISTINDEX-1>-' + histsize + '?$HISTINDEX-1:-' + histsize +'));'
-    } else {
-      var cmd = 'HISTINDEX=$((HISTINDEX+1<-1?$HISTINDEX+1:-1));'
-    }
-
-    bash.stdin.write(' ' + cmd + 'fc -l $HISTINDEX $HISTINDEX | sed -e "s/^[0-9]\\+\\t\\?\\s\\?//"\n' + boundary);
+    bash.history(direction, callback);
   });
 
   socket.on('disconnect', function () {
     logger.session(session).info('Disconnection from ' + socket.request.connection.remoteAddress, {session: session});
-
-    if (bash.connected) {
-      bash.stdin.write(' history -a\n');
-    }
+    bash.close();
   });
-
-  bash.stdin.write(boundary);
 });
 
 process.on('exit', function (code) {
